38b4873bf20f87e4ebfeeaa4ab8b1352d6d82aff
David Makaryan <97620223+WindyLibra@users.noreply.github.com>
2024-10-24 18:42:48 +0400

    Added basic functionality

---
+++ README.md
@@ -0,0 +1,23 @@
+# Typenshow
+
+Typenshow (Portmanteau for Type & Show) is a minimalistic Android app for quickly typing something down. \
+NOT INTENDED for storing information (although it will be a possibility in future), only for typing and showing the text for any purposes. \
+
+## FAQ
+
+### What state the app is in?
+
+0.0.1 Alpha. Although the app is very niche, I (the maintainer) still want to add some features
+
+### What's on the roadmap of updates?
+
+Here's the list:
+1. Add Settings widget for changing fonts and their sizes
+2. Randomize starting text (for fun, why not)
+3. Add Theming for the Background and Text
+
+### How to contribute?
+
+This is my first open-source project, so I don't know all the guidelines I should follow. \
+Nevertheless, you can write to aerolibra1196@gmail.com for questions or open an Issue. \
+If you are seeing this on GitHub, go and check the GitLab repo, as GitHub is the mirror of this project.
--- app/src/main/java/com/example/typenshow/MainActivity.kt
+++ app/src/main/java/com/example/typenshow/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.typenshow.ui.theme.TypenshowTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TypenshowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    TextInputField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(paddingValues)
+                            .padding(16.dp)
                     )
+
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+/*
+ TODO 1. Add Settings for changing fonts and their sizes
+      2. Randomize starting text
+      3. Add Theming for the Background and Text
+      4. Publish it to F-Droid
+ */
 
-@Preview(showBackground = true)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    TypenshowTheme {
-        Greeting("Android")
-    }
+fun TextInputField(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        modifier = modifier,
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontFamily = FontFamily.Default
+        ),
+        placeholder = { androidx.compose.material3.Text("Type something...") }
+    )
 }
